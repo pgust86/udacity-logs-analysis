a7814e927d8eb486ad7d95aa19b012cd3f261503
Peter Gust <eagles157@gmail.com>
2018-05-21 13:01:17 -0400

    finished 3rd Query

--- news.py
+++ news.py
@@ -30,29 +30,25 @@ def most_popular_authors():
     )
 
     #query 3 view 1 to get amount of errors per day
- CREATE OR REPLACE VIEW errors AS
- SELECT date(time), COUNT(status) AS num FROM log
- WHERE status != '200 OK'
- GROUP BY date (time);
-    #query 3 view 2 to get amount of errors per day
-  CREATE OR REPLACE VIEW total_log AS
-  SELECT date(time), COUNT(status) AS num FROM log
-  WHERE status != '200 OK'
-  GROUP BY date (time);
+#On what days did more than 1% of requests lead to errors
+def errors():
+    db = psycopg.connect(database=news)
+    c.db.cursor()
+    c.execute(
+    """WITH errors AS(
+       SELECT date(time), COUNT(status) AS err FROM log
+       WHERE status != '200 OK'
+       GROUP BY date (time)),
+       total AS(
+       SELECT date(time) AS day, COUNT(status) AS num from log
+       GROUP BY date (time)),
+       e_rate AS(
+       SELECT total.day,
+       ((err / num::float) * 100) AS eperc
+       FROM errors, total
+       WHERE errors.date = total.day)
+       SELECT * FROM e_rate WHERE eperc > 1;"""
 
-select date from total_log
-JOIN errors ON errors.date = total_log.date
-errors.num / total_log.num AS error_pct
-FROM errors, total_log
-WHERE errors.date=total_log.date;
-
-WITH errors AS(
-SELECT date(time), COUNT(status) AS err FROM log
-WHERE status != '200 OK'
-GROUP BY date (time)),
-total AS(
-SELECT date(time), COUNT(status) AS num from log
-GROUP BY date (time))
-SELECT ((err / num::float) * 100) as e_rate
-FROM errors, total
-WHERE errors.date = total.date;
+print (most_popular_articles)
+print (most_popular_authors)
+print (errors)
